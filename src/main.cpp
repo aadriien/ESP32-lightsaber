c105010fcc3d51184cf43bf773f0afdd57edf1ce
aadriien <adriienlynch@gmail.com>
2025-06-12 12:01:50 -0400

    Fixed indentation following migration

--- src/main.cpp
+++ src/main.cpp
@@ -39,146 +39,146 @@ void deactivateLightsaber();
 
 
 void setup() {
-  Serial.begin(9600);
-  delay(3000);  // Startup delay
+    Serial.begin(9600);
+    delay(3000);  // Startup delay
 
-  // Prep LEDs for output
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-    .setCorrection(TypicalLEDStrip)
-    .setDither(BRIGHTNESS < 255);
-  FastLED.setBrightness(BRIGHTNESS);
+    // Prep LEDs for output
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+        .setCorrection(TypicalLEDStrip)
+        .setDither(BRIGHTNESS < 255);
+    FastLED.setBrightness(BRIGHTNESS);
 
-  pinMode(JOY_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(JOY_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  int joyX = analogRead(JOY_X_PIN);
-  int joyY = analogRead(JOY_Y_PIN);
-  
-  checkClicked(joyX, joyY);
+    int joyX = analogRead(JOY_X_PIN);
+    int joyY = analogRead(JOY_Y_PIN);
+    
+    checkClicked(joyX, joyY);
 
-  // Read joystick changes & update color while lightsaber is ON
-  if (isActivated) {
-    if (!colorLocked) {
-      CRGB newColor;
-      if (getColorFromJoystick(joyX, joyY, newColor)) {
-        currentColor = newColor;
-        Serial.println("Color updated");
-      }
+    // Read joystick changes & update color while lightsaber is ON
+    if (isActivated) {
+        if (!colorLocked) {
+            CRGB newColor;
+            if (getColorFromJoystick(joyX, joyY, newColor)) {
+                currentColor = newColor;
+                Serial.println("Color updated");
+            }
+        }
+
+        fill_solid(leds, NUM_LEDS, currentColor);
+        FastLED.show();
     }
-
-    fill_solid(leds, NUM_LEDS, currentColor);
-    FastLED.show();
-  }
 
   delay(50);  // Smooth operation
 }
 
 
 void printColor(const CRGB& color) {
-  Serial.print("Activated Color - R: ");
-  Serial.print(color.r);
-  Serial.print(" G: ");
-  Serial.print(color.g);
-  Serial.print(" B: ");
-  Serial.println(color.b);
+    Serial.print("Activated Color - R: ");
+    Serial.print(color.r);
+    Serial.print(" G: ");
+    Serial.print(color.g);
+    Serial.print(" B: ");
+    Serial.println(color.b);
 }
 
 
 bool getColorFromJoystick(int joyX, int joyY, CRGB &outColor) {
-  int centerX = 2048;
-  int centerY = 2048;
+    int centerX = 2048;
+    int centerY = 2048;
 
-  int dx = joyX - centerX;
-  int dy = centerY - joyY;
+    int dx = joyX - centerX;
+    int dy = centerY - joyY;
 
-  float distance = sqrt(dx * dx + dy * dy);
-  float maxDistance = 2048.0;
-  float normDist = distance / maxDistance;
+    float distance = sqrt(dx * dx + dy * dy);
+    float maxDistance = 2048.0;
+    float normDist = distance / maxDistance;
 
-  // Only update color if joystick button pushed enough
-  if (normDist < 0.2) {
-    return false;  // Stay with last color
-  }
+    // Only update color if joystick button pushed enough
+    if (normDist < 0.2) {
+        return false;  // Stay with last color
+    }
 
-  // Calculate color wheel presentation based on joystick X/Y
-  float angle = atan2(dy, dx);
-  if (angle < 0) angle += 2 * PI;
+    // Calculate color wheel presentation based on joystick X/Y
+    float angle = atan2(dy, dx);
+    if (angle < 0) angle += 2 * PI;
 
-  uint8_t hue = map((int)(angle * 180 / PI), 0, 360, 0, 255);
-  uint8_t saturation = constrain(normDist * 255, 0, 255);
-  uint8_t brightness = 255;
+    uint8_t hue = map((int)(angle * 180 / PI), 0, 360, 0, 255);
+    uint8_t saturation = constrain(normDist * 255, 0, 255);
+    uint8_t brightness = 255;
 
-  outColor = CHSV(hue, saturation, brightness);
-  return true;
+    outColor = CHSV(hue, saturation, brightness);
+    return true;
 }
 
 
 bool isJoystickOutsideDeadzone(int joyX, int joyY) {
-  // Ensure we don't get stuck in default white color while joystick centered
-  int centerX = 2048;
-  int centerY = 2048;
-  
-  int dx = joyX - centerX;
-  int dy = joyY - centerY;
-  
-  int distSq = dx*dx + dy*dy;
-  return distSq > (DEADZONE_RADIUS * DEADZONE_RADIUS);
+    // Ensure we don't get stuck in default white color while joystick centered
+    int centerX = 2048;
+    int centerY = 2048;
+    
+    int dx = joyX - centerX;
+    int dy = joyY - centerY;
+    
+    int distSq = dx*dx + dy*dy;
+    return distSq > (DEADZONE_RADIUS * DEADZONE_RADIUS);
 }
 
 
 void checkClicked(int joyX, int joyY) {
-  static bool lastButtonState = HIGH;
-  bool currentButtonState = digitalRead(JOY_BUTTON_PIN);
+    static bool lastButtonState = HIGH;
+    bool currentButtonState = digitalRead(JOY_BUTTON_PIN);
 
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    if (!isActivated) {
-      // Turn on lightsaber
-      activateLightsaber(currentColor);
-      isActivated = true;
-      colorLocked = false;
-      
-      Serial.println("Lightsaber activated!");
-      printColor(currentColor);
-    } 
-    else {
-      // Lightsaber is on: toggle color lock if joystick pushed
-      if (isJoystickOutsideDeadzone(joyX, joyY)) {
-        colorLocked = !colorLocked;
-        
-        Serial.println(colorLocked ? "Color locked!" : "Color unlocked!");
-        if (colorLocked) printColor(currentColor);
-        
-      } 
-      else {
-        // Otherwise, turn off lightsaber
-        deactivateLightsaber();
-        isActivated = false;
-        colorLocked = false;
-        
-        Serial.println("Lightsaber deactivated!");
-      }
+    if (lastButtonState == HIGH && currentButtonState == LOW) {
+        if (!isActivated) {
+            // Turn on lightsaber
+            activateLightsaber(currentColor);
+            isActivated = true;
+            colorLocked = false;
+            
+            Serial.println("Lightsaber activated!");
+            printColor(currentColor);
+        } 
+        else {
+            // Lightsaber is on: toggle color lock if joystick pushed
+            if (isJoystickOutsideDeadzone(joyX, joyY)) {
+                colorLocked = !colorLocked;
+                
+                Serial.println(colorLocked ? "Color locked!" : "Color unlocked!");
+                if (colorLocked) printColor(currentColor);
+                
+            } 
+            else {
+                // Otherwise, turn off lightsaber
+                deactivateLightsaber();
+                isActivated = false;
+                colorLocked = false;
+                
+                Serial.println("Lightsaber deactivated!");
+            }
+        }
     }
-  }
-  lastButtonState = currentButtonState;
+    lastButtonState = currentButtonState;
 }
 
 
 void activateLightsaber(CRGB color) {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = color;
-    FastLED.show();
-    delay(20);
-  }
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = color;
+        FastLED.show();
+        delay(20);
+    }
 }
 
 
 void deactivateLightsaber() {
-  for (int i = NUM_LEDS - 1; i >= 0; i--) {
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(20);
-  }
+    for (int i = NUM_LEDS - 1; i >= 0; i--) {
+        leds[i] = CRGB::Black;
+        FastLED.show();
+        delay(20);
+    }
 }
 
 
